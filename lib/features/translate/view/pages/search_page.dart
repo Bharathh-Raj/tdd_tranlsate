10ee49dfb242496e5e4127b06b4b42ee5cef8697
Bharath <bt9944@gmail.com>
2021-12-18 19:42:45 +0530

    UI: search_page.dart

---
+++ lib/features/translate/view/pages/search_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  late TextEditingController textEditingController;
+
+  @override
+  void initState() {
+    textEditingController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Translate'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextField(
+              controller: textEditingController,
+              textInputAction: TextInputAction.go,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Translate"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
